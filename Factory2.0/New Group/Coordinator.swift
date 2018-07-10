009c3a30c4d6f895a5bbd027c9f27017b29d09a1
Mateo Došlić <mateo55@live.com>
2018-07-10 10:11:35 +0200

    Coordinator - test

---
+++ Factory2.0/New Group/Coordinator.swift
@@ -0,0 +1,28 @@
+//
+//  Coordinator.swift
+//  Factory2.0
+//
+//  Created by Mateo Došlić on 09/07/2018.
+//  Copyright © 2018 Mateo Došlić. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+
+public protocol Coordinator: class{
+    var childCoordinator: [Coordinator] {get set}
+    var presenter: UINavigationController {get set}
+
+    func start()
+}
+
+public extension Coordinator {
+    func addChildCoordinator(childCoordinator: Coordinator){
+        self.childCoordinator.append(childCoordinator)
+    }
+    func removeChildCoordinator(childCoordinator: Coordinator){
+        self.childCoordinator = self.childCoordinator.filter { $0 !== childCoordinator }
+    }
+    
+}
